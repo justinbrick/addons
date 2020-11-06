86b41a84c42567b2caf08fedc1b67b979b48fa74
justinbrick <justinbrick1@gmail.com>
2020-11-05 22:41:07 -0500

    Boilerplate Code

--- Quicksilver/lua/weapons/weapon_quicksilver.lua
+++ Quicksilver/lua/weapons/weapon_quicksilver.lua
@@ -23,3 +23,19 @@ SWEP.Slot = 2
 SWEP.SlotPos = 1
 SWEP.DrawAmmo = false
 SWEP.DrawCrosshair = false
+
+--Function firing upon left click.
+function SWEP:PrimaryAttack()
+    self:SetNextPrimaryFire(CurTime() + 1) --We want a 1 second delay between firing.
+    print("Primary Fire!")
+end
+
+--Function firing upon right click.
+function SWEP:SecondaryAttack()
+    self:SetNextSecondaryFire(CurTime() + 1) --TODO: Change timer based on what I want this function to do.
+end
+
+--The main function we'll use to "slow" time.
+function SWEP:SlowTime()
+
+end
