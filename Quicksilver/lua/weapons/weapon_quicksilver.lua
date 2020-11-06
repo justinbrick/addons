660c4404421d1af31c008528dc7b3bed8183310f
justinbrick <justinbrick1@gmail.com>
2020-11-05 22:29:55 -0500

    first commit

---
+++ Quicksilver/lua/weapons/weapon_quicksilver.lua
@@ -0,0 +1,25 @@
+--Basic SWEP Setup
+
+SWEP.PrintName = "Quicksilver"
+SWEP.Author = "POTATO#9375"
+SWEP.Instructions = "Left click to slow down time."
+SWEP.Spawnable = true
+SWEP.AdminOnly = true
+
+SWEP.Primary.ClipSize = -1
+SWEP.Primary.DefaultClip = -1
+SWEP.Primary.Automatic = false
+SWEP.Primary.Ammo = "none"
+
+SWEP.Secondary.ClipSize = -1
+SWEP.Secondary.DefaultClip = -1
+SWEP.Secondary.Automatic = false
+SWEP.Secondary.Ammo = "none"
+
+SWEP.Weight = 5
+SWEP.AutoSwitchTo = false
+SWEP.AutoSwitchFrom = false
+SWEP.Slot = 2
+SWEP.SlotPos = 1
+SWEP.DrawAmmo = false
+SWEP.DrawCrosshair = false
